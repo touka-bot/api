7a46fb3060e1afe84f00ccdcb0fe09b34b63ec65
shinixsensei <crezoxplays@gmail.com>
2021-01-24 23:19:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,8 @@ var bodyParser = require("body-parser");
 const cors = require('cors');
 var app = express();
 
-let whitelist = ['http://client.4c3711.xyz/']
-
 app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin 
-        if (!origin) return callback(null, true);
-        if (whitelist.indexOf(origin) === -1) {
-            var message = `The CORS policy for this origin doesn't ` +
-            `allow access from the particular origin.`;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
+    origin: 'http://client.4c3711.xyz/'
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
